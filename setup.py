07295e44b674cd1af2855cebd9fca16291a406df
Friedrich Lindenberg <friedrich@pudo.org>
2016-07-23 14:28:30 +0200

    allow 0/1 for booleans

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='typecast',
-    version='0.2.5',
+    version='0.2.6',
     description="Convert types in source data.",
     long_description="",
     classifiers=[
